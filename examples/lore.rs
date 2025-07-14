233aebf2eb4cf736e0d8e4ceaf8a6f067ac4f71b
OJarrisonn <j.h.m.t.v.10@gmail.com>
2025-07-14 11:30:59 -0300

    Example lore usage
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>

---
+++ examples/lore.rs
@@ -0,0 +1,21 @@
+use ph::api::lore::LoreApi;
+use ph::config::Config;
+use ph::log::Log;
+use ph::net::Net;
+
+#[tokio::main]
+async fn main() {
+    let config = Config::mock(Default::default());
+    let log = Log::mock();
+    let net = Net::spawn(config, log);
+
+    let lore = LoreApi::spawn(net);
+
+    let lists = lore.get_available_lists().await.unwrap();
+    println!("Total mailing lists: {}", lists.len());
+    let item = lists.get(0).unwrap();
+    let name = item.name.clone();
+    println!("First mailing list: {}", name);
+    let patch_feed = lore.get_patch_feed(name, 0).await.unwrap();
+    println!("Patch feed: {}", patch_feed);
+}
