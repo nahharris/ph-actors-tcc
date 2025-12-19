47fc37a046d9ff9cc8b9a10509d306db1a41692e
Hannah Harrisonn <hannah@harrisonn.dev>
2025-12-19 12:20:04 -0300

    m(tese): revisão cap4
    Signed-off-by: Hannah Harrisonn <hannah@harrisonn.dev>

--- monografia/latex/conteudo/codigo/rust-var-mutabilidade.rs
+++ monografia/latex/conteudo/codigo/rust-var-mutabilidade.rs
@@ -2,8 +2,8 @@ fn main() {
     let a = 10;
     let mut b = 20;
     
-    // a += 1; // Proibido: a não é mutável
-    b += 1; // Permitido: b é mutável
+    // a += 1; // Proibido: a imutável
+    b += 1; // Permitido: b mutável
     
     // Quando o código termina aqui, a e b são liberados da memória
 }
